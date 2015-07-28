61a00eb60653d6e4a4cef0afc548b9e27ab375a3
rudygarrido <gar14366@uvg.edu.gt>
2015-07-28 08:01:44 -0600

    Aasd

--- ADTStack.java
+++ ADTStack.java
@@ -1,4 +1,13 @@
-
+/*
+Universidad del Valle de Guatemala
+Algoritmos y Estructura de Datos
+Sección: 10
+Diego Morales. Carné: 14012
+Diego Sosa. Carné: 14735
+Julio González. Carné: 14096
+30/07/2015
+Hoja de Trabajo 2
+*/
 
 public interface ADTStack<E> {
 	public void empty();
