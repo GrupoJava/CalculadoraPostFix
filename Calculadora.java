9d01700fbb2f710f6dbb72a6ad42396007cf260a
rudygarrido <gar14366@uvg.edu.gt>
2015-07-29 19:19:42 -0600

    Modss

---
+++ Calculadora.java
@@ -0,0 +1,17 @@
+import java.io.FileNotFoundException;
+
+public class Calculadora implements ADTCalculadora{
+
+	@Override
+	public void readFile(String file) throws FileNotFoundException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Object calcular() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
--- InterfazGrafica.java
+++ InterfazGrafica.java
@@ -1,8 +1,19 @@
 import java.awt.EventQueue;
 
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JSlider;
+
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import javax.swing.JTextArea;
+import javax.swing.JTextPane;
 
 
 public class InterfazGrafica {
@@ -10,6 +21,11 @@ public class InterfazGrafica {
 		private JFrame frame;
 		private JSlider slider;
 		private JLabel label;
+		private JTextField textField;
+		private JFileChooser fc;
+		private File file;
+		private JButton btnSeleccionarArchivo;
+		private JButton buttonCalcular;
 		public static void main(String[] args) {
 			EventQueue.invokeLater(new Runnable() {
 				public void run() {
@@ -28,8 +44,59 @@ public class InterfazGrafica {
 		}
 		public void initialize(){
 			frame = new JFrame();
-			frame.setBounds(100, 100, 622, 515);
+			frame.setBounds(100, 100, 622, 381);
 			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			frame.getContentPane().setLayout(null);
+			
+			JLabel lblCalculadoraPostfix = new JLabel("Calculadora Postfix");
+			lblCalculadoraPostfix.setFont(new Font("Tahoma", Font.BOLD, 50));
+			lblCalculadoraPostfix.setBounds(10, 11, 586, 106);
+			frame.getContentPane().add(lblCalculadoraPostfix);
+			
+			btnSeleccionarArchivo = new JButton("Seleccionar Archivo");
+			btnSeleccionarArchivo.setBounds(422, 128, 174, 23);
+			btnSeleccionarArchivo.addActionListener(new Evento());
+			frame.getContentPane().add(btnSeleccionarArchivo);
+			
+			textField = new JTextField();
+			textField.setBounds(20, 128, 378, 23);
+			frame.getContentPane().add(textField);
+			textField.setColumns(10);
+			
+			buttonCalcular = new JButton("Calcular");
+			buttonCalcular.setBounds(20, 164, 174, 23);
+			frame.getContentPane().add(buttonCalcular);
+			
+			JLabel lblResultados = new JLabel("Resultados:");
+			lblResultados.setBounds(20, 199, 100, 21);
+			frame.getContentPane().add(lblResultados);
+			
+			JTextArea textArea = new JTextArea();
+			textArea.setBounds(20, 218, 378, 113);
+			frame.getContentPane().add(textArea);
+			
+			fc = new JFileChooser();
+			
+			
+		}
+		private class Evento implements ActionListener{
+
+			
+
+			
+
+			/* (non-Javadoc)
+			 * @see java.awt.event.ActionListener#actionPerformed(java.awt.event.ActionEvent)
+			 */
+			public void actionPerformed(ActionEvent a) {
+				int returnVal = fc.showOpenDialog(frame);
+				
+		        if (returnVal == JFileChooser.APPROVE_OPTION) {
+		            file = fc.getSelectedFile();
+		            textField.setText(file.getAbsolutePath());
+		        } else {
+		            
+		        }
+			}
 		}
 }
